2d0726dd4a6b3c70206074a316003079381481f9
sergicollado <sergicollado@gmail.com>
2021-06-05 17:05:37 +0200

    feat(json_processor): check parameters on tests

--- tests/json_processor_test.py
+++ tests/json_processor_test.py
@@ -3,6 +3,7 @@ from unittest.mock import Mock
 from message_client import MessageClient
 from json_processor import json_processor
 
+
 @pytest.fixture
 def input_json():
     filename = 'tests/fixture_base.json'
@@ -11,17 +12,46 @@ def input_json():
     json_file.close()
 
 
-def test_should_send_post_message(input_json):
+def test_should_send_post_message_with_proper_params(input_json):
     MessageClient.send_post = Mock()
     json_processor(input_json)
-    MessageClient.send_post.assert_called()
+    expected_url = 'http: //example.com/push/'
+    expected_data = {
+        "name": "John Doe 1",
+        "email": None,
+        "phone": None,
+        "url": "http: //example.com/push/",
+        "type": "post"
+    }
+    MessageClient.send_post.assert_called_once_with(expected_url,
+                                                    expected_data)
 
-def test_should_send_sms_message(input_json):
+
+def test_should_send_sms_message_with_proper_params(input_json):
     MessageClient.send_sms = Mock()
     json_processor(input_json)
-    MessageClient.send_sms.assert_called()
 
-def test_should_send_email_message(input_json):
+    expected_phone = '+34692018264'
+    expected_data = {
+        "name": "John Doe 3",
+        "email": None,
+        "phone": "+34692018264",
+        "url": None,
+        "type": "sms"
+    }
+    MessageClient.send_sms.assert_called_once_with(expected_phone, expected_data)
+
+
+def test_should_send_email_message_with_proper_params(input_json):
     MessageClient.send_email = Mock()
     json_processor(input_json)
-    MessageClient.send_email.assert_called()
+
+    expected_email = 'doe@example.com'
+    expected_data = {
+    "name": "John Doe 2",
+    "email": "doe@example.com",
+    "phone": None,
+    "url": "http: //example.com",
+    "type": "email"
+    }
+    MessageClient.send_email.assert_called_once_with(expected_email, expected_data)
