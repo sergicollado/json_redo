f4daadca9393f11cab3eb7e7adce5299de9f90d1
sergicollado <sergicollado@gmail.com>
2021-06-05 14:44:22 +0200

    feat(json_processor): process post items

--- json_processor.py
+++ json_processor.py
@@ -1,7 +1,11 @@
 import ijson
-from itertools import islice
+from message_client import MessageClient
 
 def json_processor(json):
-    objects = ijson.items(f, 'item')
-    objects = islice(objects, 200)
-    print(objects)
+    for entry_log in ijson.items(json, 'item'):
+        process_post(entry_log)
+
+def process_post(data: dict):
+    if data['type'] != 'post':
+        return
+    MessageClient.send_post(data['url'], data)
--- tests/json_processor_test.py
+++ tests/json_processor_test.py
@@ -4,22 +4,15 @@ from message_client import MessageClient
 from json_processor import json_processor
 
 @pytest.fixture
-def enter_json():
-    return '''[
-        {
-        name: "John Doe 1",
-        email: null,
-        phone: null,
-        url: http://example.com/push/,
-        type: "post",
-        }
-    ]'''
-
-
-
-def test_from_enter_json_must_call_proper_message_client_method(enter_json):
+def input_json():
     filename = 'tests/fixture_base.json'
-    message_client = Mock()
-    with open(filename) as json_file:
-        json_processor(json_file)
-    message_client.send_post.assert_called()
+    json_file = open(filename, "rb")
+    yield json_file
+    json_file.close()
+
+
+def test_from_enter_json_must_call_proper_message_client_method(input_json):
+    
+    MessageClient.send_post = Mock()
+    json_processor(input_json)
+    MessageClient.send_post.assert_called()
